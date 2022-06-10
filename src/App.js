2cdba7ae3da44bdd5d4a64cb376d2a2e2cc27134
suveiko <suveikosasha@gmail.com>
2022-06-10 16:10:05 +0400

    Created Skeleton for PizzaBlock

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import {useEffect, useState} from "react";
 import {Header} from "./components/Header";
 import {Categories} from "./components/Categories";
 import {Sort} from "./components/Sort";
-import {PizzaBlock} from "./components/PizzaBlock";
+import {PizzaBlock} from "./components/PizzaBlock/PizzaBlock";
+import {Skeleton} from "./components/PizzaBlock/Skeleton";
 
 import './scss/app.scss'
 
 function App() {
 
     const [pizzas, setPizzas] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         fetch('https://62a304695bd3609cee6048e1.mockapi.io/pizzas')
             .then(res => res.json())
-            .then(arr => setPizzas(arr))
+            .then(arr => {
+                    setPizzas(arr)
+                    setIsLoading(false)
+                }
+            )
     }, [])
 
     return (
@@ -30,12 +36,10 @@ function App() {
                         <h2 className="content__title">Все пиццы</h2>
                         <div className="content__items">
                             {
-                                pizzas.map(t => (
-                                    <PizzaBlock
-                                        key={t.id}
-                                        {...t}
-                                    />
-                                ))}
+                                isLoading
+                                    ? [...new Array(10)].map((_, index) => <Skeleton key={index}/>)
+                                    : pizzas.map(t => (<PizzaBlock key={t.id}{...t}/>
+                                    ))}
                         </div>
                     </div>
                 </div>
---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+export const Skeleton = () => (
+        <ContentLoader
+            className='pizza-block'
+            speed={2}
+            width={280}
+            height={465}
+            viewBox="0 0 280 465"
+            backgroundColor="#f3f3f3"
+            foregroundColor="#ecebeb"
+        >
+            <circle cx="140" cy="137" r="120" />
+            <rect x="0" y="276" rx="19" ry="19" width="280" height="27" />
+            <rect x="0" y="318" rx="11" ry="11" width="280" height="88" />
+            <rect x="60" y="455" rx="0" ry="0" width="1" height="0" />
+            <rect x="114" y="419" rx="25" ry="25" width="167" height="45" />
+            <rect x="0" y="424" rx="14" ry="14" width="89" height="30" />
+        </ContentLoader>
+)
