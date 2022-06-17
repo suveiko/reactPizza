38e4254652c5569bead852c3515b9bc600746ada
suveiko <suveikosasha@gmail.com>
2022-06-17 14:51:22 +0400

    feat: add Redux Toolkit and file store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 
+import {store} from "./redux/store";
 import App from './App';
 
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +11,12 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </BrowserRouter>
-);
+)
+;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
